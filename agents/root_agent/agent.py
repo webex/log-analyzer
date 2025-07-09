73a3652f02a7c5899b28634e4ba3afbc77437900
Sarang S <email.sarangs@gmail.com>
2025-07-09 10:05:38 +0530

    chore: add optik observability

--- agents/root_agent/agent.py
+++ agents/root_agent/agent.py
@@ -1,19 +1,24 @@
 import os
 from google.adk.agents import SequentialAgent
+from opik.integrations.adk import OpikTracer, track_adk_agent_recursive
 
 os.environ["AZURE_OPENAI_API_KEY"] = (
-    "MDczZDFmMDQtNDkwMC00ZDM2LWI1OGItY2YzMDlhZDMyYzA2N2U4YWRhNGEtM2U1_A52D_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+    "MTk1ZjBmZjMtMTczZS00ZTFlLWE2N2EtN2I2MDVlMWMzZDgyZjRhODgxYWItNWNl_A52D_1eb65fdf-9643-417f-9974-ad72cae0e10f"
 )
 os.environ["AZURE_OPENAI_ENDPOINT"] = (
     "https://llm-proxy.us-east-2.int.infra.intelligence.webex.com/azure/v1"
 )
-os.environ["AZURE_API_VERSION"] = "2024-12-01-preview"
+os.environ["AZURE_API_VERSION"] = "2024-10-21"
 
 from analyze_agent.agent import analyze_agent
 from search_agent.agent import search_agent
+
+opik_tracer = OpikTracer()
 
 root_agent = SequentialAgent(
     name="MicorserviceLogAnalyzerAgent",
     sub_agents=[search_agent, analyze_agent],
     description="Executes a sequence of log searching and analysis agents.",
 )
+
+track_adk_agent_recursive(root_agent, opik_tracer)
