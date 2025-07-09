29151aef068e8a0bb338741dc81b70e60f79b16f
Sarang S <email.sarangs@gmail.com>
2025-07-09 10:06:07 +0530

    fix: update to gpt 4.1 for 1M context

--- agents/analyze_agent/agent.py
+++ agents/analyze_agent/agent.py
@@ -4,14 +4,17 @@ from google.adk.models.lite_llm import LiteLlm
 
 analyze_agent = LlmAgent(
     model=LiteLlm(
-        model="azure/gpt-4o",
+        model="azure/gpt-4.1",
         api_key=os.environ["AZURE_OPENAI_API_KEY"],
         api_base=os.environ["AZURE_OPENAI_ENDPOINT"],
         api_version=os.environ["AZURE_API_VERSION"],
     ),
     name="analyze_agent",
-    instruction='''Analyze the following JSON data and provide insights about logs. 
-    Provide temporal analysis highlighting different endpoints and segregation of microservices. 
-    If any errors are present, provide its detailed analysis:
+    instruction='''You are a network analyst expert in http, webRTC, SIP, SDP and other related protocols. 
+    Analyze the following logs and provide detailed insights in layman terms. 
+    Provide temporal analysis highlighting critical endpoints. 
+    Provide segregation of all the various microservices like wdm, mobius, cpapi, mercury being used in the sessions. 
+    If any errors are present in the logs, provide its detailed analysis, including potential causes and fixes for a user
+
     {search_results}''',
 )
