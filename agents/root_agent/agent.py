be33af78d16842fb4e6ff0c0d943e414a2159cc7
lgoel <lgoel@cisco.com>
2025-07-21 12:37:59 +0530

    update root agent

--- agents/root_agent/agent.py
+++ agents/root_agent/agent.py
@@ -1,9 +1,8 @@
 import os
 from google.adk.agents import SequentialAgent
-from opik.integrations.adk import OpikTracer, track_adk_agent_recursive
 
 os.environ["AZURE_OPENAI_API_KEY"] = (
-    "NjYxMGQ4NmItNDIwYi00MDNhLWI0MTUtMjVjODk2N2ViZjE2NGYwNjAwYjUtMzE0_A52D_1eb65fdf-9643-417f-9974-ad72cae0e10f"
+    "YmUxMzhkNDUtYmIwZS00NmI1LTgxODEtZDI3YjljZmU4MWY5OGNmZWI2NjQtYTcz_A52D_1eb65fdf-9643-417f-9974-ad72cae0e10f"
 )
 os.environ["AZURE_OPENAI_ENDPOINT"] = (
     "https://llm-proxy.us-east-2.int.infra.intelligence.webex.com/azure/v1"
@@ -14,12 +13,9 @@ from analyze_agent.agent import analyze_agent
 from search_agent.agent import search_agent
 from visualAgent.agent import sequence_diagram_agent
 
-opik_tracer = OpikTracer()
-
 root_agent = SequentialAgent(
     name="MicorserviceLogAnalyzerAgent",
     sub_agents=[search_agent, analyze_agent, sequence_diagram_agent],
     description="Executes a sequence of log searching and analysis agents.",
 )
 
-track_adk_agent_recursive(root_agent, opik_tracer)
